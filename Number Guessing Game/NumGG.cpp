5fa145de11df952103a282f4a687094703dabbaf
HIMANSHU PRASAD <133243721+Himanshu17032002@users.noreply.github.com>
2024-09-15 20:16:19 +0530

    Add files via upload

---
+++ Number Guessing Game/NumGG.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    int guess = 0;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a number between 1 and 100. Try to guess it!" << endl;
+
+    // Loop until the user guesses the correct number
+    while (guess != randomNumber) {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the number correctly!" << endl;
+        }
+    }
+
+    return 0;
+}
